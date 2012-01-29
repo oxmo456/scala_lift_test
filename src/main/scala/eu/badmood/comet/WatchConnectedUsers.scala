738a9680f13d138c2fe76d06deda66b32b2a3cb4
oxmo456 <nicolas.anto.markovic@gmail.com>
2012-01-29 10:18:47 +0100

    removed timespan as it is not guaranteed to be accurate

--- src/main/scala/eu/badmood/comet/WatchConnectedUsers.scala
+++ src/main/scala/eu/badmood/comet/WatchConnectedUsers.scala
@@ -1,15 +1,11 @@
 package eu.badmood.comet
 
 import net.liftweb.http.CometActor
-import net.liftweb.util.Helpers._
-import net.liftweb.common.Full
 import xml.Text
 import eu.badmood.avsbgame.AvsBGameStats
 import eu.badmood.avsbgame.AvsBGameStats.{UserConnected, UserDisconnected}
 
 class WatchConnectedUsers extends CometActor{
-
-  override def lifespan = Full(10 seconds)
 
   override def localSetup(){
     super.localSetup()
