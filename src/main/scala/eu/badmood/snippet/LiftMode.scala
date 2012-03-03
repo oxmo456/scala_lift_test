13c14aa1618a43dfb239baf1760d2cf417f0956b
oxmo456 <nicolas.anto.markovic@gmail.com>
2012-03-03 14:40:54 +0100

    turned LiftMode snippet into an object DispatchSnippet

--- src/main/scala/eu/badmood/snippet/LiftMode.scala
+++ src/main/scala/eu/badmood/snippet/LiftMode.scala
@@ -2,7 +2,13 @@ package eu.badmood.snippet
 
 import net.liftweb.util.Helpers._
 import net.liftweb.util.Props
+import net.liftweb.http.DispatchSnippet
 
-class LiftMode {
+object LiftMode extends DispatchSnippet {
+
+  def dispatch = {
+    case "render" => render
+  }
+
   def render = "*" #> ("(" + Props.mode.toString + ")")
 }
