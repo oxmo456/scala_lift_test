93a15713081f52a7a7c543d188eddf3cff3d76a3
oxmo456 <nicolas.anto.markovic@gmail.com>
2012-03-03 14:05:30 +0100

    turned MicroChatInput snippet into a DispatchSnippet

--- src/main/scala/eu/badmood/snippet/MicroChatInput.scala
+++ src/main/scala/eu/badmood/snippet/MicroChatInput.scala
@@ -1,12 +1,15 @@
 package eu.badmood.snippet
 
 import net.liftweb.util.Helpers._
-import net.liftweb.http.SHtml
 import eu.badmood.avsbgame.MicroChat
-import xml.{Text, NodeSeq}
-import net.liftweb.http.js.JsCmds.{SetValById, SetValueAndFocus, SetHtml, Alert}
+import net.liftweb.http.js.JsCmds.{SetValById}
+import net.liftweb.http.{DispatchSnippet, SHtml}
 
-class MicroChatInput {
+class MicroChatInput extends DispatchSnippet{
+
+  def dispatch = {
+    case "render" => render
+  }
 
   def render = "name=microChatInput" #> SHtml.onSubmit(s => {
     MicroChat ! MicroChat.Message(s)
